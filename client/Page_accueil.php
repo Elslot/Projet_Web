85aec003fc452627f175d7c2f04d14a2e3981fe0
Corentin Beurier <corentin.beurier@etu.u-bordeaux.fr>
2017-01-05 09:18:41 +0100

    Démarrage

---
+++ client/Page_accueil.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+ <head>
+    <meta charset=\" utf-8\">
+    <title>Accueil</title>
+ </head>
+ <body>
+  <ul>
+      <li>Accueil</li>
+      <li>Catalogue</li>
+      <li>Mon compte</li>
+      <li>A propos</li>
+  </ul>
+ </body>
+</html>
